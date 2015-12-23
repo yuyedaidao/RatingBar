4181f336f8a08c66e2a70b47be1ee1f729c0fc1b
Oleg Melnik <olezko46@gmail.com>
2015-12-23 22:11:40 +0300

    Update RatingBar.h

--- RatingBar/RatingBar.h
+++ RatingBar/RatingBar.h
@@ -9,21 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @protocol RatingDelegate
+
 @optional
 -(void) setRating:(NSInteger)rating isHuman:(BOOL) isHuman;
+
 @end
 
 @interface RatingBar : UIView
-@property (nonatomic,assign) NSInteger starNumber;
-/*
- *调整底部视图的颜色
- */
-@property (nonatomic,strong) UIColor *viewColor;
 
-/*
- *是否允许可触摸
- */
-@property (nonatomic,assign) BOOL enable;
-
+@property (nonatomic, assign) NSInteger starNumber;
+@property (nonatomic, strong) UIColor *viewColor;
+@property (nonatomic, assign) BOOL enable;
 @property (nonatomic, assign) id  delegate;
+
 @end
+
